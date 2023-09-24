e15c14d48ab16c2e4771ed37b00ccbbf0f6ad5e6
Mercy Cherono <cheronom142@gmail.com>
2023-09-24 11:12:49 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+//Using a for loop to return messages in an array
+let messagesArray = [];
+const namesArray = ["Mercy", "Faith", "Brenda"];
+let eventName;
+
+function writeCards(
+  namesArray = ["Mercy", "Faith", "Brenda"],
+  eventName = "birthday"
+) {
+  for (let i = 0; i < namesArray.length; i++) {
+    let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+
+    messagesArray.push(message);
+  }
+  return messagesArray;
+}
+
+//Using while loop to create a count down.
+function countDown(number) {
+  let i = number;
+  while (i <= number && i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
+countDown();
